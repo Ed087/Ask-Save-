80ab9aee79a45950ddb4150f14538a31da58e892
Ed087 <128875634+Ed087@users.noreply.github.com>
2025-04-11 07:51:59 +0200

    Aktualisieren von App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-1import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [activeQuestions, setActiveQuestions] = useState(
